b67ac18d5724e8e2649c452c4d7ea925dd842623
Lorenz Weber <mail@lenzw.de>
2013-04-30 17:56:42 +0200

    added database table "organism" to cli too db.phar

---
+++ cli/src/db/tables/Organism.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace cli_db;
+
+require_once ROOT . 'classes/AbstractTable.php';
+
+class Organism extends AbstractTable {
+
+    public static function getKeys() {
+        return array(
+            'id' => array(
+                'colname' => 'OrganismId',
+                'actions' => array(
+                    'details' => 'required',
+                    'update' => 'required',
+                    'delete' => 'required',
+                ),
+                'description' => 'organism id'
+            ),
+            'abbreviation' => array(
+                'colname' => 'Abbreviation',
+                'actions' => array(
+                    'insert' => 'optional',
+                    'update' => 'optional',
+                ),
+                'description' => 'e.g. H.sapiens'
+            ),
+            'genus' => array(
+                'colname' => 'Genus',
+                'actions' => array(
+                    'insert' => 'required',
+                    'update' => 'required',
+                ),
+                'description' => 'e.g. Homo'
+            ),
+            'species' => array(
+                'colname' => 'Species',
+                'actions' => array(
+                    'insert' => 'required',
+                    'update' => 'required',
+                ),
+                'description' => 'e.g. sapiens'
+            ),
+            'common_name' => array(
+                'colname' => 'CommonName',
+                'actions' => array(
+                    'insert' => 'required',
+                    'update' => 'required',
+                ),
+                'description' => 'e.g. human'
+            ),
+            'comment' => array(
+                'colname' => 'Comment',
+                'actions' => array(
+                    'insert' => 'optional',
+                    'update' => 'optional',
+                ),
+                'description' => 'some comment'
+            ),
+        );
+    }
+
+    public static function CLI_commandDescription() {
+        return 'Manipulate the database organism.';
+    }
+
+    public static function CLI_commandName() {
+        return 'organism';
+    }
+
+    public static function CLI_longHelp() {
+        
+    }
+
+    public static function getSubCommands() {
+        return array('insert', 'update', 'delete', 'details', 'list');
+    }
+
+    public static function executeCommand($options, $command_name) {
+        $keys = self::getKeys();
+        switch ($command_name) {
+            case 'insert':
+                self::command_insert($options, $keys);
+                break;
+            case 'update':
+                self::command_update($options, $keys);
+                break;
+            case 'delete':
+                self::command_delete($options, $keys);
+                break;
+            case 'details':
+                self::command_details($options, $keys);
+                break;
+            case 'list':
+                self::command_list($options, $keys);
+                break;
+        }
+    }
+
+    public static function getPropelClass() {
+        return '\\cli_db\\propel\\Organism';
+    }
+
+}
+
+?>
