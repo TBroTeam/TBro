05e5dda1c80c4dc881451820c88e0217f9109836
Markus Ankenbrand <markus.ankenbrand@stud-mail.uni-wuerzburg.de>
2013-07-23 16:55:42 +0200

    Importer_Sequences_FASTA angepasst um peptide zu übernehmen

--- src/cli/import/commands/Importer_Sequences_FASTA.php
+++ src/cli/import/commands/Importer_Sequences_FASTA.php
@@ -64,7 +64,7 @@ class Importer_Sequences_FASTA extends AbstractImporter {
         $predpeps_added = 0;
 
         #pre-initialize variables to bind statement parameters
-        $param_isoform_uniq = null;
+        $param_isoform_name = null;
         $param_isoform_seqlen = null;
         $param_isoform_residues = null;
 
@@ -84,31 +84,12 @@ class Importer_Sequences_FASTA extends AbstractImporter {
             # prepare statements
             #
             #insert sequence into existing isoform
-            $statement_update_isoform = $db->prepare('UPDATE feature SET (seqlen, residues) = (:seqlen, :residues) WHERE uniquename=:uniquename AND organism_id=:organism RETURNING feature_id');
-            $statement_update_isoform->bindParam('uniquename', $param_isoform_uniq, PDO::PARAM_STR);
+            $statement_update_isoform = $db->prepare('UPDATE feature SET (seqlen, residues) = (:seqlen, :residues) WHERE name=:name AND organism_id=:organism AND type_id=:type_id RETURNING feature_id');
+            $statement_update_isoform->bindParam('name', $param_isoform_name, PDO::PARAM_STR);
             $statement_update_isoform->bindParam('seqlen', $param_isoform_seqlen, PDO::PARAM_INT);
             $statement_update_isoform->bindParam('residues', $param_isoform_residues, PDO::PARAM_STR);
             $statement_update_isoform->bindValue('organism', DB_ORGANISM_ID, PDO::PARAM_INT);
-
-
-            #create predicted peptide
-            $statement_insert_predpep = $db->prepare('INSERT INTO feature  (type_id, organism_id, name, uniquename, seqlen, residues, dbxref_id) '
-                    . 'VALUES (:type_id, :organism_id, :name, :uniquename, :seqlen, :residues, :dbxref_id) RETURNING feature_id');
-            $statement_insert_predpep->bindValue('type_id', CV_PREDPEP, PDO::PARAM_INT);
-            $statement_insert_predpep->bindValue('organism_id', DB_ORGANISM_ID, PDO::PARAM_INT);
-            $statement_insert_predpep->bindParam('name', $param_predpep_name, PDO::PARAM_STR);
-            $statement_insert_predpep->bindParam('uniquename', $param_predpep_uniq, PDO::PARAM_STR);
-            $statement_insert_predpep->bindParam('seqlen', $param_predpep_seqlen, PDO::PARAM_INT);
-            $statement_insert_predpep->bindParam('residues', $param_predpep_residues, PDO::PARAM_STR);
-            $statement_insert_predpep->bindValue('dbxref_id', $import_prefix_id, PDO::PARAM_INT);
-
-            #link predpep to parent isoform
-            $statement_insert_predpep_location = $db->prepare(sprintf('INSERT INTO featureloc (fmin, fmax, strand, feature_id, srcfeature_id) VALUES (:fmin, :fmax, :strand, :feature_id, (%s))', 'SELECT feature_id FROM feature WHERE uniquename=:srcfeature_uniquename LIMIT 1'));
-            $statement_insert_predpep_location->bindParam('fmin', $param_predpep_fmin, PDO::PARAM_INT);
-            $statement_insert_predpep_location->bindParam('fmax', $param_predpep_fmax, PDO::PARAM_INT);
-            $statement_insert_predpep_location->bindParam('strand', $param_predpep_strand, PDO::PARAM_INT);
-            $statement_insert_predpep_location->bindParam('feature_id', $param_predpep_feature_id, PDO::PARAM_INT);
-            $statement_insert_predpep_location->bindParam('srcfeature_uniquename', $param_predpep_srcfeature_uniq, PDO::PARAM_STR);
+            $statement_update_isoform->bindValue('type_id', Importer_Sequence_Ids::get_import_dbxref(), PDO::PARAM_INT);
 
             #read file and execute statements
 
@@ -118,29 +99,11 @@ class Importer_Sequences_FASTA extends AbstractImporter {
                 list($description, $sequence) = self::read_fasta($file);
 
                 $matches = array();
-                #predicted peptide header like this:
-                #>m.1812924 g.1812924  ORF g.1812924 m.1812924 type:5prime_partial len:376 (+) comp224705_c0_seq18:3-1130(+)
-                if (preg_match('/^>(?<id>[^\s]+) .* (?<name>[^\s]+):(?<from>\d+)-(?<to>\d+)\((?<dir>[+-])\)$/', $description, $matches)) {
-                    $param_predpep_name = $matches['id'];
-                    $param_predpep_uniq = IMPORT_PREFIX . "_" . self::prepare_predpep_name($matches['name'], $matches['from'], $matches['to'], $matches['dir']);
-                    $param_predpep_seqlen = strlen($sequence);
-                    $param_predpep_residues = $sequence;
-                    //create predpep
-                    $statement_insert_predpep->execute();
-                    //link to parent feature
-                    $param_predpep_feature_id = $statement_insert_predpep->fetchColumn();
-                    $param_predpep_srcfeature_uniq = IMPORT_PREFIX . "_" . $matches['name'];
-                    $param_predpep_fmin = min($matches['from'], $matches['to']);
-                    $param_predpep_fmax = max($matches['from'], $matches['to']);
-                    $param_predpep_strand = $matches['dir'] == '+' ? 1 : -1;
-                    $statement_insert_predpep_location->execute();
-                    $predpeps_added+=$statement_insert_predpep->rowCount();
-                }
 
                 #isoform header like this:
                 #>comp173079_c0_seq1 len=2161 path=[2139:0-732 2872:733-733 2873:734-1159 3299:1160-1160 3300:1161-1513 3653:1514-1517 3657:1518-2160]
-                else if (preg_match('/^>(?<name>[^\s]+) .*$/', $description, $matches)) {
-                    $param_isoform_uniq = IMPORT_PREFIX . "_" . $matches['name'];
+                if (preg_match('/^>(?<name>[^\s]+) .*$/', $description, $matches)) {
+                    $param_isoform_name = $matches['name'];
                     $param_isoform_seqlen = strlen($sequence);
                     $param_isoform_residues = $sequence;
                     //update isoform with values
