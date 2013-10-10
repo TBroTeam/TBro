eaf40a53b73ed0fdd1240e578b06758f2768f8ac
Lorenz Weber <mail@lenzw.de>
2013-10-10 19:13:46 +0200

    queue config tool

--- src/queue/server/queue-config-tool.php
+++ src/queue/server/queue-config-tool.php
@@ -30,9 +30,9 @@ $xmldata = <<<EOF
         <name>add_database</name>
         <description>add_database</description>
         
-        <option name="name">
-            <short_name>-n</short_name>
-            <long_name>--name</long_name>
+        <option name="dbname">
+            <short_name>-d</short_name>
+            <long_name>--dbname</long_name>
             <description>name of database file within zip archive</description>
         </option>
 
@@ -53,11 +53,53 @@ $xmldata = <<<EOF
         <name>remove_database</name>
         <description>remove_database</description>
         
-        <option name="name">
-            <short_name>-n</short_name>
-            <long_name>--name</long_name>
+        <option name="dbname">
+            <short_name>-d</short_name>
+            <long_name>--dbname</long_name>
             <description>name of database file within zip archive</description>
         </option>
+    </command>
+    
+    <command>
+        <name>list_programs</name>
+        <description>list programs</description>
+    </command>
+
+    <command>
+        <name>list_relationships</name>
+        <description>list programs with associated databases</description>
+    </command>
+    
+    <command>
+        <name>link_database_program</name>
+        <description>link program(s) to database</description>
+        
+        <option name="dbname">
+            <short_name>-d</short_name>
+            <long_name>--dbname</long_name>
+            <description>name of database file within zip archive</description>
+        </option>
+        
+       <argument name="programs">
+           <multiple>TRUE</multiple>
+           <description>programs to link</description>
+       </argument>
+    </command>
+    
+    <command>
+        <name>unlink_database_program</name>
+        <description>unlink program(s) from database</description>
+        
+        <option name="dbname">
+            <short_name>-d</short_name>
+            <long_name>--dbname</long_name>
+            <description>name of database file within zip archive</description>
+        </option>
+        
+       <argument name="programs">
+           <multiple>TRUE</multiple>
+           <description>programs to link</description>
+       </argument>
     </command>
 </command>
 EOF;
@@ -96,26 +138,78 @@ try {
                 break;
 
             case 'add_database':
-                require_command_option($result->command, 'name');
+                require_command_option($result->command, 'dbname');
                 require_command_option($result->command, 'md5');
                 require_command_option($result->command, 'uri');
 
                 $stm_ins = $pdo->prepare('INSERT INTO database_files (name, md5, download_uri) VALUES (?,?,?);');
-                $stm_ins->execute(array($result->command->options['name'], $result->command->options['md5'], $result->command->options['uri']));
+                $stm_ins->execute(array($result->command->options['dbname'], $result->command->options['md5'], $result->command->options['uri']));
                 if ($stm_ins->rowCount() > 0)
                     echo "successfully inserted\n";
 
                 break;
 
             case 'remove_database':
-                require_command_option($result->command, 'name');
+                require_command_option($result->command, 'dbname');
 
 
                 $stm_del = $pdo->prepare('DELETE FROM database_files WHERE name=?');
-                $stm_del->execute(array($result->command->options['name']));
+                $stm_del->execute(array($result->command->options['dbname']));
 
                 if ($stm_del->rowCount() > 0)
                     echo "successfully removed\n";
+
+                break;
+
+            case 'list_programs':
+                $stm_list = $pdo->prepare('SELECT name FROM programs');
+                $stm_list->execute();
+
+                $tbl = new Console_Table();
+                $tbl->setHeaders(array('name'));
+
+                while ($row = $stm_list->fetch(\PDO::FETCH_NUM))
+                    $tbl->addRow($row);
+
+                echo $tbl->getTable();
+
+                break;
+
+            case 'list_relationships':
+                $stm_list = $pdo->prepare('SELECT programname, database_name FROM program_database_relationships');
+                $stm_list->execute();
+
+                $tbl = new Console_Table();
+                $tbl->setHeaders(array('programname', 'database_name'));
+
+                while ($row = $stm_list->fetch(\PDO::FETCH_NUM))
+                    $tbl->addRow($row);
+
+                echo $tbl->getTable();
+
+                break;
+
+            case 'link_database_program':
+                require_command_option($result->command, 'dbname');
+                $dbname = $result->command->options['dbname'];
+                foreach ($result->command->args['programs'] as $program) {
+                    $stm_ins = $pdo->prepare('INSERT INTO program_database_relationships (programname, database_name) VALUES (?,?);');
+                    $stm_ins->execute(array($program, $dbname));
+                    if ($stm_ins->rowCount() > 0)
+                        echo "successfully inserted $dbname <-> $program \n";
+                }
+
+                break;
+
+            case 'unlink_database_program':
+                require_command_option($result->command, 'dbname');
+                $dbname = $result->command->options['dbname'];
+                foreach ($result->command->args['programs'] as $program) {
+                    $stm_ins = $pdo->prepare('DELETE FROM program_database_relationships  WHERE (programname, database_name) = (?,?);');
+                    $stm_ins->execute(array($program, $dbname));
+                    if ($stm_ins->rowCount() > 0)
+                        echo "successfully removed $dbname <-> $program \n";
+                }
 
                 break;
         }
@@ -127,7 +221,7 @@ try {
     if (defined('DEBUG') && DEBUG) {
         throw $exc;
     }
-    $parser->displayError("\n".$exc->getMessage()."\n\n");
+    $parser->displayError("\n" . $exc->getMessage() . "\n\n");
 }
 
 function require_command_option($command, $optionname) {
@@ -135,4 +229,5 @@ function require_command_option($command, $optionname) {
         die("Please specify command line option $optionname !\n");
     }
 }
+
 ?>
