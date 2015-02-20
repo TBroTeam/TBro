83a865da4183acf0e822ebe1d2130f5ee6f91913
Markus Ankenbrand <markus@ankenbrand.me>
2015-02-20 10:10:39 +0100

    Hide diffexp heatmap in case of no data found. also alert user

--- src/web/includes/TranscriptDB/smarty/templates/js/feature_diffexp.js
+++ src/web/includes/TranscriptDB/smarty/templates/js/feature_diffexp.js
@@ -45,50 +45,56 @@ function getFilterData() {
 
 //display barplot
 $('#button-draw-diffexp-heatmap').click(function () {
+    $('#diffexp-heatmap-panel').hide(200);
     $.ajax('{#$ServicePath#}/graphs/heatmap/diffexp', {
         method: 'post',
         data: getFilterData(),
         success: function (val) {
-            $('#diffexp-heatmap-panel').show(0);
-            var parent = $("#diffexp-heatmap-canvas-parent");
+            if (val.y.smps.length > 0) {
 
-            //if we already have an old canvas, we have to clean that up first
-            var canvas = $('#diffexp-heatmap-canvas');
-            var cx = canvas.data('canvasxpress');
-            if (cx != null) {
-                cx.destroy();
-                parent.empty();
+                $('#diffexp-heatmap-panel').show(0);
+                var parent = $("#diffexp-heatmap-canvas-parent");
+
+                //if we already have an old canvas, we have to clean that up first
+                var canvas = $('#diffexp-heatmap-canvas');
+                var cx = canvas.data('canvasxpress');
+                if (cx != null) {
+                    cx.destroy();
+                    parent.empty();
+                }
+
+                canvas = $('<canvas id="diffexp-heatmap-canvas"></canvas>');
+                parent.append(canvas);
+                canvas.attr('width', parent.width() - 8);
+                canvas.attr('height', 500);
+
+                window.location.hash = "diffexp-heatmap-panel";
+
+                val.y.names = [];
+                for (var i = 0; i < val.y.data.length; i++) {
+                    val.y.names[i] = val.y.vars[i];
+                }
+
+                cx = new CanvasXpress(
+                        "diffexp-heatmap-canvas",
+                        {
+                            "x": val.x,
+                            "y": val.y
+                        },
+                {
+                    graphType: "Heatmap",
+                    showDataValues: true,
+                    graphOrientation: "vertical",
+                    zoomSamplesDisable: true,
+                    zoomVariablesDisable: true
+                });
+
+                canvas.data('canvasxpress', cx);
+
+                // addTable(parent, val);
+            } else {
+                alert("No differential expression data found for this feature/quantification/analysis combination.");
             }
-
-            canvas = $('<canvas id="diffexp-heatmap-canvas"></canvas>');
-            parent.append(canvas);
-            canvas.attr('width', parent.width() - 8);
-            canvas.attr('height', 500);
-
-            window.location.hash = "diffexp-heatmap-panel";
-
-            val.y.names = [];
-            for (var i = 0; i < val.y.data.length; i++) {
-                val.y.names[i] = val.y.vars[i];
-            }
-
-            cx = new CanvasXpress(
-                    "diffexp-heatmap-canvas",
-                    {
-                        "x": val.x,
-                        "y": val.y
-                    },
-            {
-                graphType: "Heatmap",
-                showDataValues: true,
-                graphOrientation: "vertical",
-                zoomSamplesDisable: true,
-                zoomVariablesDisable: true
-            });
-
-            canvas.data('canvasxpress', cx);
-
-            // addTable(parent, val);
         }
     });
     return false;
