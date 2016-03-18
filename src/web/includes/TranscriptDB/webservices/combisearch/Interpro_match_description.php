b6793730163a24af8ecb667b94d4a590082b47e7
Markus Ankenbrand <markus@ankenbrand.me>
2016-03-18 13:35:00 +0100

    Add id restriction capability to search for interpro description

--- src/web/includes/TranscriptDB/webservices/combisearch/Interpro_match_description.php
+++ src/web/includes/TranscriptDB/webservices/combisearch/Interpro_match_description.php
@@ -23,6 +23,13 @@ class Interpro_match_description extends \WebService {
         $species = $querydata['species'];
         $release = $querydata['release'];
 
+        $ids = array();
+        $ids_query = "";
+        if (isset($querydata['ids']) && count($querydata['ids']) > 0) {
+            $ids = $querydata['ids'];
+            $ids_query = 'AND feature.feature_id IN (' . implode(',', array_fill(0, count($ids), '?')) . ')';
+        }
+
         $term = strtoupper(sprintf('%%%s%%', trim($querydata['term'])));
 
         $query_get_features = <<<EOF
@@ -30,15 +37,15 @@ class Interpro_match_description extends \WebService {
 SELECT feature.feature_id FROM feature, 
 	(SELECT featureloc.srcfeature_id FROM featureloc, 
 		(SELECT DISTINCT srcfeature_id FROM featureloc, 
-			(SELECT feature_id FROM featureprop WHERE type_id={$constant('CV_INTERPRO_ANALYSIS_MATCH_DESCRIPTION')} AND UPPER(value) LIKE :term) AS fi
+			(SELECT feature_id FROM featureprop WHERE type_id={$constant('CV_INTERPRO_ANALYSIS_MATCH_DESCRIPTION')} AND UPPER(value) LIKE ?) AS fi
 		WHERE featureloc.feature_id = fi.feature_id) AS fp 
 	WHERE featureloc.feature_id=fp.srcfeature_id) as fl
 WHERE feature.feature_id = fl.srcfeature_id
 AND feature.type_id IN ({$constant('CV_ISOFORM')}, {$constant('CV_UNIGENE')})
-AND feature.organism_id = :species 
+AND feature.organism_id = ? 
 AND feature.dbxref_id = 
-	(SELECT dbxref_id FROM dbxref WHERE db_id={$constant('DB_ID_IMPORTS')} AND accession=:release LIMIT 1)
-        
+	(SELECT dbxref_id FROM dbxref WHERE db_id={$constant('DB_ID_IMPORTS')} AND accession=? LIMIT 1)
+{$ids_query}
 EOF;
         
 //        $query = "SELECT name FROM feature WHERE id=?";
@@ -49,11 +56,7 @@ EOF;
 
         $data = array('results' => array());
 
-        $stm_get_features->execute(array(
-            'term' => $term,
-            'species' => $species,
-            'release' => $release
-        ));
+        $stm_get_features->execute(array_merge(array($term, $species, $release), $ids));
         
         while ($row = $stm_get_features->fetch(PDO::FETCH_ASSOC)) {
             $data['results'][] = $row['feature_id'];
